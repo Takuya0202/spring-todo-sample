b1ca0f4a4940ce73a95639d169244af9d2172c11
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-06-22 02:51:05 +0900

    sampleのマイグレーション完了

---
+++ todo-sample/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into todos (todo , detail , create_at , updated_at)
+ values ('springの勉強' , 'todoアプリでハンズオン' , CURRENT_TIMESTAMP , CURRENT_TIMESTAMP);
+
+insert into todos (todo , detail , create_at , updated_at)
+ values ('dddの勉強' , 'ドメインオブジェクトについて考える' , CURRENT_TIMESTAMP , CURRENT_TIMESTAMP);
